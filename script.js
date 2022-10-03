0e8657070b63fe0e19d7c61314d74126ad8ca999
lilia hern <lhern222@ucr.edu>
2022-10-03 16:45:17 -0700

    password criteria

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment Code
+var specials = "!@#$%^&*()?><:";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+
+var all = specials + lowercase + uppercase + numbers;
+
+amountCharacters = prompt("Choose a length of characters from 8-128.")
+  if (amountCharacters < 8) {
+    console.log ("try a larger number.");
+  } else if (amountCharacters > 128) {
+    console.log ("try a smaller number.");
+  } else {
+    alert("your password will be exactly " + amountCharacters + " characters long.");
+  }
+
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +29,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//writePassword was changed to generatePassword
